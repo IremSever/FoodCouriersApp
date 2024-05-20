32bdf1dfa0778dbb930c82136d983bdb5ce24c7a
Harun Orhan <ce.harunorhan@gmail.com>
2024-05-21 00:07:16 +0300

    ChatUIEvent and DataState is written

--- app/src/main/java/com/irem/foodcouriersapp/viewmodel/event/ChatUIEvent.kt
+++ app/src/main/java/com/irem/foodcouriersapp/viewmodel/event/ChatUIEvent.kt
@@ -1,4 +1,13 @@
 package com.irem.foodcouriersapp.viewmodel.event
 
-class ChatUIEvent {
+import android.graphics.Bitmap
+
+sealed class ChatUIEvent {
+
+    data class UpdatePrompt(val newPrompt: String): ChatUIEvent()
+
+    data class SendPrompt(
+        val prompt: String,
+        val bitmap: Bitmap?
+    ) : ChatUIEvent()
 }
--- app/src/main/java/com/irem/foodcouriersapp/viewmodel/state/DataState.kt
+++ app/src/main/java/com/irem/foodcouriersapp/viewmodel/state/DataState.kt
@@ -1,2 +1,14 @@
 package com.irem.foodcouriersapp.viewmodel.state
 
+import android.graphics.Bitmap
+import java.util.Date
+
+data class DataState(
+    val chatList: MutableList<Chat> = mutableListOf(),
+    val prompt : String = "",
+    val bitmap: Bitmap? = null,
+    val isAwaitingName: Boolean = true
+
+)
+
+data class Chat(val message: String, val bitmap: Bitmap?, val isUser: Boolean, val sentTime: Date = Date())
